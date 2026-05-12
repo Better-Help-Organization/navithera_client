429c1cf96278e3d54c068d62860e22628dcf3534
Israel <israelmandefro165@gmail.com>
2026-05-13 00:14:16 +0300

    modified:   lib/core/localization/providers/locale_provider.dart

--- lib/core/localization/providers/locale_provider.dart
+++ lib/core/localization/providers/locale_provider.dart
@@ -1,25 +1,30 @@
 // lib/core/providers/locale_provider.dart
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:navithera_client/feature/auth/presentation/providers/auth_provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
+final secureStorageProvider = Provider<FlutterSecureStorage>((ref) {
+  return const FlutterSecureStorage();
+});
 final localeProvider = StateNotifierProvider<LocaleNotifier, Locale>((ref) {
-  return LocaleNotifier(ref.read(sharedPreferencesProvider));
+  return LocaleNotifier(ref.read(secureStorageProvider));
 });
 
 class LocaleNotifier extends StateNotifier<Locale> {
-  final SharedPreferences _prefs;
+  final FlutterSecureStorage _storage;
 
-  LocaleNotifier(this._prefs) : super(_loadLocale(_prefs));
+  LocaleNotifier(this._storage) : super(const Locale('en')) {
+    _loadLocale();
+  }
 
-  static Locale _loadLocale(SharedPreferences prefs) {
-    final languageCode = prefs.getString('languageCode') ?? 'en';
-    return Locale(languageCode);
+  Future<void> _loadLocale() async {
+    final languageCode = await _storage.read(key: 'languageCode') ?? 'en';
+    state = Locale(languageCode);
   }
 
   Future<void> setLocale(Locale locale) async {
-    await _prefs.setString('languageCode', locale.languageCode);
+    await _storage.write(key: 'languageCode', value: locale.languageCode);
     state = locale;
   }
 }
